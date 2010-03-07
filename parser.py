e91e26a9fde8fd9deb6f5057acae23a8b4aeb79f
Enrico Tröger <enrico.troeger@uvena.de>
2010-03-07 23:20:14 +0100

    Implement support for parsing container node user_beancounter files

--- parser.py
+++ parser.py
@@ -18,7 +18,6 @@ class VzUbcMonParser(object):
     def __init__(self, filename):
         self._filename = filename
         self._bc_version = None
-        self._container_id = None
         self._resources = None
 
     #----------------------------------------------------------------------
@@ -43,17 +42,16 @@ class VzUbcMonParser(object):
         if self._resources is None:
             self._parse()
 
-        return (self._bc_version, self._container_id)
+        return self._bc_version
 
     #----------------------------------------------------------------------
     def _parse(self):
         """
         Parse /proc/user_beancounters and store the values in the object
         """
-        # TODO adjust the code to also parse more than one container so that we
-        # could also run on a container node
         ubc_file = open(self._filename, 'r')
         self._resources = {}
+        current_ctid = 0
         for line in ubc_file:
             values = line.strip().split()
             values_len = len(values)
@@ -64,14 +62,20 @@ class VzUbcMonParser(object):
                     ctid = values[0][:-1]
                     if ctid.isdigit():
                         # we got the container id
-                        self._container_id = ctid
+                        current_ctid = ctid
+                        self._resources[current_ctid] = {}
                     else:
                         # header line
                         continue
                     del values[0]
                 resource_name = values[0]
                 resource_values = values[1:] # skip the name
-                self._resources[resource_name] = VzUbcMonResource(resource_seq=resource_values)
+                self._resources[current_ctid][resource_name] = \
+                    VzUbcMonResource(resource_seq=resource_values)
 
         ubc_file.close()
 
+
+if __name__ == '__main__':
+    from pprint import pprint
+    pprint(VzUbcMonParser('sample_host').get_resources())
