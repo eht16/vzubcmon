087a0fcaac0fd125c29f8803dfb7f36c702152c2
Enrico Tröger <enrico.troeger@uvena.de>
2013-01-13 23:51:49 +0100

    Add helper utility to read UBC data

---
+++ contrib/readubc.c
@@ -0,0 +1,22 @@
+/*
+ * Based on http://www.labradordata.ca/downloads/beanc.c
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_LINE 1024
+
+int main()
+{
+	char line[MAX_LINE];
+	FILE *file = fopen("/proc/user_beancounters", "r");
+	if (file == NULL)
+		exit(1);
+
+	while (fgets(line, MAX_LINE, file))
+		printf("%s", line);
+
+	fclose(file);
+	return 0;
+}
